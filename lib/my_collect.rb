57f7c98317bbc3cd409d9e93db6df4c54b2e12ea
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-15 00:18:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,11 @@
 def my_collect(array)
- empty_array = []
+ empty_array = [] 
  counter = 0
- my_collect(empty_array) do |x| 
-   counter +=1 
+ while counter < array.length 
+ empty_array << yield(array[counter])
+ counter +=1 
+   
  end
+ empty_array
 end 
 
